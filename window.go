77ec269d0a53c369dd4c9152f3c944e30aa196ac
IoIxD <alphaproject217@gmail.com>
2022-05-23 05:30:13 -0700

    'NewWindow' can no be called without any flags

--- window.go
+++ window.go
@@ -18,8 +18,11 @@ type Window struct {
 }
 
 // Function for creating a new window, with default options in place.
-func NewWindow(Width uint16, Height uint16, Flags []uint32) (Window Window, Error error) {
-	flags := []uint32{Flags[0], Flags[1] | 
+func NewWindow(Width, Height uint16, Flags []uint32) (Window Window, Error error) {
+	var flag0, flag1 uint32
+	if(len(Flags) >= 1) {flag0 = Flags[0]}
+	if(len(Flags) >= 2) {flag0 = Flags[1]}
+	flags := []uint32{flag0, flag1 | 
 	xproto.EventMaskStructureNotify | 
 	xproto.EventMaskExposure | 
 	xproto.EventMaskPointerMotion |
@@ -31,7 +34,41 @@ func NewWindow(Width uint16, Height uint16, Flags []uint32) (Window Window, Erro
 }
 
 // Function for creating a new window without any default options in place
-func NewWindowComplex(Width uint16, Height uint16, BorderWidth uint16, Mask uint32, Flags []uint32) (win Window, err error) {
+func NewWindowComplex(Width, Height, BorderWidth uint16, Mask uint32, Flags []uint32) (win Window, err error) {
+
+	// establish a new connection
+	X, err := xgb.NewConn()
+	if err != nil {
+		return
+	}
+
+	// establish a new connection
+	setup := xproto.Setup(X)
+	screen := setup.DefaultScreen(X)
+
+	// and a new window ID
+	windowID, err := xproto.NewWindowId(X)
+	if err != nil {return}
+
+	// create a window using that ID
+	xproto.CreateWindow(X, screen.RootDepth, windowID, screen.Root,
+		0, 0, Width, Height, BorderWidth,
+		xproto.WindowClassInputOutput, screen.RootVisual, 
+		Mask,
+		Flags)
+
+	// make that window appear on screen.
+	err = xproto.MapWindowChecked(X, windowID).Check()
+	if err != nil {return}
+
+	// make a new Window object with what we've gotten here
+	win = Window{X, screen, windowID,Width,Height, 		make(chan Event)}
+
+	return
+}
+
+// Function for creating a new window without any default options in place
+func NewWindowRaw(Width, Height, BorderWidth uint16, Mask uint32, Flags []uint32) (win Window, err error) {
 
 	// establish a new connection
 	X, err := xgb.NewConn()
